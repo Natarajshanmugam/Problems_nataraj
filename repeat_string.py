47f5abb95277a176697a7115370c6a137990154e
Nataraj <50852569+Natarajshanmugam@users.noreply.github.com>
2021-04-17 12:53:02 +0530

    Add files via upload

---
+++ repeat_string.py
@@ -0,0 +1,36 @@
+##https://www.hackerrank.com/challenges/repeated-string/problem
+
+##string='kmretasscityylpdhuwjirnqimlkcgxubxmsxpypgzxtenweirknjtasxtvxemtwxuarabssvqdnktqadhyktagjxoanknhgilnm'
+##k=736778906400
+
+##k=872514961806
+##string='udjlitpopjhipmwgvggazhuzvcmzhulowmveqyktlakdufzcefrxufssqdslyfuiahtzjjdeaxqeiarcjpponoclynbtraaawrps'
+
+##k=10
+##string='aba'
+
+import math
+count=1
+final=0
+count_of_a = string.count('a')
+print("First a occurences",count_of_a)
+if(len(string)>1):
+    if(k-len(string) == len(string)):
+        print(2*count_of_a)
+    else:
+        multiplier=((k-len(string))//len(string))
+        count=(count+multiplier)*count_of_a
+        if(k%len(string) == 0):
+            print("direct",count)
+        else:
+            count+=string[0:k%len(string)].count('a')
+            print("inside mod",count)
+            
+            
+else:
+    if(string.count('a')>0):
+        print(k)
+    else:
+        print(string.count('a'))
+        
+    
